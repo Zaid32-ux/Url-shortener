7b3b6e33c37d1a7618d76eb7a5a1dc78e81fc8d5
Zaid32-ux <Zgour5071@gmail.com>
2026-05-04 21:45:16 +0530

    setup api routing with /api base path

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import  dbConnection  from "./database/dbConnection.js";
+import apiRoutes from "./routes/api.js";
+import Url from "./models/Url.js";
 
 //“ config Load my secret variables from this file so I can use process.env.”
 import { config } from "dotenv";
@@ -8,7 +10,8 @@ import { config } from "dotenv";
 config({ path: "./.env" });//calling again in main file bcz load it globally its recommended
 const app = express();
 
-app.use(express.json());
+app.use("/api", apiRoutes);
 
+app.use(express.json());
   dbConnection();
 export default app;
