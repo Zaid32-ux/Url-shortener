0414fe2a31ff23c18f4fcdfe0b77c6e4853d21de
Zaid32-ux <Zgour5071@gmail.com>
2026-05-07 00:36:26 +0530

    add URL controller with basic structure

---
+++ backend/controllers/urlController.js
@@ -0,0 +1,12 @@
+import { nanoid } from "nanoid";
+import Url from "../models/Url.js";
+
+// Create shortened URL
+export const createShortUrl = async (req, res) => {
+ 
+};
+
+// Get all URLs
+export const getAllUrls = async (req, res) => {
+ 
+};
