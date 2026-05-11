dce12041203874cad8892e540a0bd1b52a850a1d
Zaid32-ux <Zgour5071@gmail.com>
2026-05-11 11:39:56 +0530

    configure CORS for local frontend connection

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import  dbConnection  from "./database/dbConnection.js";
 import urlRoutes from "./routes/urlRoutes.js";
 import Url from "./models/Url.js";
+import cors from "cors";
 
 //“ config Load my secret variables from this file so I can use process.env.”
 import { config } from "dotenv";
@@ -9,6 +10,13 @@ import { config } from "dotenv";
 
 config({ path: "./.env" });//calling again in main file bcz load it globally its recommended
 const app = express();
+
+// CORS configuration for frontend connection
+const corsOptions = {
+  origin: ["http://localhost:5173"], // frontend URL
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
 app.use("/api", apiRoutes);
 
